fe65ea4a94b6d67b9a123f2f2a96488b07ba3a84
Zachary Barnes <zachhsavage@gmail.com>
2023-03-25 15:05:59 -0400

    Added some notes/comments to sllow direction in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //variables for misc
 var weatherIcon = document.querySelector("#weather-icon");
 var searchForm = document.querySelector("#search-form");
-var searchInput = document.querySelector("#city-search");
+var citySearch = document.querySelector("#city-search");
 
 //variables for current data
 var currentHeading = document.querySelector("#current-heading");
@@ -15,3 +15,27 @@ var errorContainer = document.querySelector("#error-container");
 var temp = document.querySelector("#temp");
 var wind = document.querySelector("#wind");
 var humid = document.querySelector("#humid");
+
+
+
+// * * * Get Current Weather Information * * * //
+
+
+
+// * * * Display Current Weather Information * * * //
+
+
+
+// * * * Get Forecast Weather Information * * * //
+
+
+
+// * * * Display Forecast Weather Information * * * //
+
+
+
+// * * * Save Search History * * * //
+
+
+
+// * * * Load Search History * * * //
