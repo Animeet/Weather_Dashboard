8fd115d9ce7e28999dd73d0c91c5d42e25ef471b
Zachary Barnes <zachhsavage@gmail.com>
2023-03-24 23:47:52 -0400

    Added some variables to start off my javascript

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//variables for misc
+var weatherIcon = document.querySelector("#weather-icon");
+var searchForm = document.querySelector("#search-form");
+var searchInput = document.querySelector("#city-search");
+
+//variables for current data
+var currentHeading = document.querySelector("#current-heading");
+var currentData = document.querySelector("#current-data");
+var currentIcon = document.querySelector("#current-icon");
+var clearButton = document.querySelector("#clear-btn");
+var searchContainer = document.querySelector("#search-container")
+var errorContainer = document.querySelector("#error-container");
+
+//variables for card data
+var temp = document.querySelector("#temp");
+var wind = document.querySelector("#wind");
+var humid = document.querySelector("#humid");
